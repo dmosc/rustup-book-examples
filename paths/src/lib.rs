b6ceaa026d9088d88cd500bc0e4a82711adab53b
dmosc <osdavrm@gmail.com>
2022-12-06 16:59:36 -0600

    Segment code into mods.

--- paths/src/lib.rs
+++ paths/src/lib.rs
@@ -1,37 +1,5 @@
-mod back_of_house {
-    #[derive(Debug)]
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-}
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+mod back_of_house;
+mod front_of_house;
 
 mod customer {
     // If this is imported outside `customer` module, we could refer to the
