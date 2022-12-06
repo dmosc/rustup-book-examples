5a2f560da055fdd4a8b58e0b8b068bfd98aef09c
dmosc <osdavrm@gmail.com>
2022-12-06 16:08:58 -0600

    Add `use` to shorten paths.

--- paths/src/lib.rs
+++ paths/src/lib.rs
@@ -1,4 +1,5 @@
 mod back_of_house {
+    #[derive(Debug)]
     pub enum Appetizer {
         Soup,
         Salad,
@@ -32,11 +33,20 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd {} like toast please", meal.toast);
+mod customer {
+    // If this is imported outside `customer` module, we could refer to the
+    // modules as well using `super::Appetizer` for example.
+    use crate::back_of_house::{Appetizer, Breakfast};
+    use crate::front_of_house::hosting;
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+        let mut meal = Breakfast::summer("Rye");
+        meal.toast = String::from("Wheat");
+        println!("I'd {} like toast please", meal.toast);
+
+        let order1 = Appetizer::Soup;
+        let order2 = Appetizer::Salad;
+        println!("{:?}\n{:?}", order1, order2);
+    }
 }
