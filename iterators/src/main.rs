166ca0f5a07a608e5d87eb305a833e1c7147f3c8
dmosc <osdavrm@gmail.com>
2022-12-16 16:57:56 -0600

    Add iterator consumer/producer methods.

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -4,8 +4,15 @@ fn main() {
     for val in v1_iter {
         println!("{val}");
     }
+
+    // Methods that consume iterators.
+    //
     // v1_iter can't be used since it was moved
     // and used in the previous for-loop.
     let total: i32 = v1.iter().sum();
     println!("{total}");
+
+    // Methods that produce other iterators -- .map(...).
+    let v1_x2: Vec<i32> = v1.iter().map(|v| v * 2).collect();
+    println!("{:?}", v1_x2);
 }
