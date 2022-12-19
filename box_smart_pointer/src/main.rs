0db8be9a340bfa5529139858e644ebd10b7ec44e
dmosc <osdavrm@gmail.com>
2022-12-18 19:55:49 -0600

    Add dereferencing coercion example.

--- box_smart_pointer/src/main.rs
+++ box_smart_pointer/src/main.rs
@@ -1,7 +1,24 @@
+use std::ops::Deref;
+
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        Self(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 fn main() {
@@ -10,4 +27,16 @@ fn main() {
         Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
     );
     println!("{:?}", list);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let name = MyBox::new(String::from("John"));
+    deref_coerce_hello(&name);
+}
+
+fn deref_coerce_hello(name: &str) {
+    println!("Hello, {name}");
 }
